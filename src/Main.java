7a27760b21cced86b80dd9b0f4785c319041273e
Raahim Fareed <raahim1965@hotmail.com>
2023-05-20 12:55:44 +0500

    Added another println

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Raahim is mad");
+        System.out.println("This is a test line");
     }
 }
