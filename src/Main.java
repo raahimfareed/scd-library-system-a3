2bd536cafc7056594c3a05699b597772523d8ff3
Raahim Fareed <raahim1965@hotmail.com>
2023-05-23 15:44:32 +0500

    changed default form to signup

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,6 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
         Database.initialize();
-        Login_Form frame = new Login_Form();
-        frame.setVisible(true);
-        frame.setTitle("Log-In");
-        frame.pack();
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(299,300);
-        frame.setLocationRelativeTo(null); // Center the frame
+        Signup signupForm = new Signup();
     }
 }
